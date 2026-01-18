119775b602af219c7331ffb37cc081becdf9b645
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2026-01-18 21:31:52 +0800

    feat: make assets work in home profile

--- lib/main.dart
+++ lib/main.dart
@@ -13,27 +13,25 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 244, 244, 244),
         appBar: PreferredSize(
-          preferredSize: Size.fromHeight(60),
+          preferredSize: Size.fromHeight(72),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 24, 0, 0),
+            padding: const EdgeInsets.fromLTRB(0, 12, 0, 0),
             child: AppBar(
-              title: Container(
-                height: 36,
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 174, 103, 103),
-                  borderRadius: BorderRadius.circular(
-                    6,
-                  ), // remove for sharp rectangle
+              title: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.transparent,
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/prof.png"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Rectangle inside AppBar",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
               ),
             ),
           ),
