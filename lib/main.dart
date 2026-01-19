66a1b37b10aad69cc1029413712f7555b33de1be
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2026-01-19 21:46:18 +0800

    feat: partially make the navbar
    it now changes tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'analytics.dart';
+import 'home.dart';
+import 'plants.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,34 +12,101 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 244, 244, 244),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(72),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 12, 0, 0),
-            child: AppBar(
-              title: CircleAvatar(
-                radius: 20,
-                backgroundColor: Colors.transparent,
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.white, width: 2),
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/prof.png"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _index = 0;
+  final List<Widget> _screens = const [
+    HomeTab(),
+    AnalyticsScreen(),
+    PlantsScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 244, 244, 244),
+      body: _screens[_index],
+      appBar: const ProfileAppBar(),
+      bottomNavigationBar: MyBottomNav(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+      ),
+    );
+  }
+}
+
+class ProfileAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const ProfileAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(72);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 12, 0, 0),
+      child: AppBar(
+        title: CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.transparent,
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.white, width: 2),
+              image: const DecorationImage(
+                image: AssetImage("assets/images/prof.png"),
+                fit: BoxFit.cover,
               ),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const MyBottomNav({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.pie_chart),
+          label: 'Analytics',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.energy_savings_leaf_rounded),
+          label: 'Plants',
+        ),
+      ],
     );
   }
 }
---
+++ lib/plants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlantsScreen extends StatelessWidget {
+  const PlantsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Plants Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
